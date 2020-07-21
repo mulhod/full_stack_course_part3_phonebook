54c213b6cc865c5b6c1351197bd4043619ac8765
Matthew Mulholland <mmulholland@ets.org>
2020-07-20 23:09:58 -0400

    Update scripts to include frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
