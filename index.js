15af9ce62d098eb146074e836598a2665556cd36
Matthew Mulholland <mmulholland@ets.org>
2020-08-03 23:16:22 -0400

    part3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+require('dotenv').config()
 const morgan = require('morgan')
+const Entry = require('./models/entry')
 const cors = require('cors');
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
@@ -11,24 +13,6 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  }
-]
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
@@ -37,63 +21,43 @@ app.get('/info', (req, res) => {
     res.send(`<div>Phonebook has info for ${persons.length} people</div><br/><div>${Date()}</div>`)
 })
   
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Entry.find({}).then(entries => {
+    response.json(entries)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Entry.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+/* app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-  return maxId + 1
-}
+}) */
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body)
 
-  if (!body.name || ! body.number) {
-    return response.status(400).json({ 
-      error: 'Missing name/number' 
-    })
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
-  if (persons.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({
-      error: `${body.name} already exists!`
-    })
-  }
-
-  const person = {
+  const entry = new Entry({
     name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  entry.save().then(savedEntry => {
+    response.json(savedEntry)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
