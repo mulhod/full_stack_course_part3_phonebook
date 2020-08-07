4ca1465acb50010b9ef9e86996b36c95d1205512
Matthew Mulholland <mmulholland@ets.org>
2020-08-06 22:58:07 -0400

    part3.15: Phonebook database, step3 and part3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 const app = express()
 
-app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/', (req, res) => {
@@ -27,18 +27,24 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Entry.findById(request.params.id).then(person => {
-    response.json(person)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-/* app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
-}) */
+app.delete('/api/persons/:id', (request, response, next) => {
+  Entry.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -56,6 +62,18 @@ app.post('/api/persons', (request, response) => {
     response.json(savedEntry)
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
